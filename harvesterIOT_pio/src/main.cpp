8cac4d4efab3dc0286950deb31fd0030099b121e
misikovich <misikovich.dev@gmail.com>
2026-03-22 19:42:56 +0100

    feat: Add initial project structure including audio assets, pinout documentation, and a basic Arduino sketch with pin definitions.

--- harvesterIOT_pio/src/main.cpp
+++ harvesterIOT_pio/src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BLINK_R_MOSFET_PIN 4
+#define BLINK_L_MOSFET_PIN 3
+#define SW_R_BLINK_PIN 0
+#define SW_L_BLINK_PIN 1
+#define MTR_DRV8871_IN1_PIN 21
+#define MTR_DRV8871_IN2_PIN 20
+#define AUD_MAX98357_I2S_LRC_PIN 5
+#define AUD_MAX98357_I2S_BCLK_PIN 6
+#define AUD_MAX98357_I2S_DIN_PIN 7
+#define ACCEL_ADXL345_SDA_PIN 8
+#define ACCEL_ADXL345_SCL_PIN 9
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
